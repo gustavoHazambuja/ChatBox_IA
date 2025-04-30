a8b22a15f22636cf45a86e420c2dfc16b9189b88
Gustavo Henrique <guazambujinha@gmail.com>
2025-04-30 17:02:31 -0300

    integração API

---
+++ chatbox/src/main/java/com/example/Entitie/GeminiClient.java
@@ -0,0 +1,10 @@
+package com.example.Entitie;
+
+public class GeminiClient {
+    
+    private static final String API_KEY = "AIzaSyB8wAgUd90VVrSkHv-LhYvaYCOUs242zQc";
+    private static final String ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY;
+
+
+    
+}
